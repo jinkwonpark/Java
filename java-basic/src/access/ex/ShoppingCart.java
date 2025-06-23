39e2c7ca7533b8408eec02fe0587575f25f3e3fb
jinkwonpark <pjk3473@gmail.com>
2025-06-23 19:25:39 +0900

    Section 06. 접근 제어자

---
+++ java-basic/src/access/ex/ShoppingCart.java
@@ -0,0 +1,34 @@
+package access.ex;
+
+public class ShoppingCart {
+    private Item[] items = new Item[10];
+    private int itemCount;
+
+    public void addItem(Item item) {
+        if (itemCount >= items.length) {
+            System.out.println("장바구니가 가득 찼습니다.");
+            return;
+        }
+        items[itemCount] = item;
+        itemCount++;
+        // items[itemCount++] = item;  // 위의 두 줄을 이렇게 생략 가능, 후위 증감연산자
+    }
+
+    public void displayItems() {
+        System.out.println("장바구니 상품 출력");
+        for (int i = 0; i < itemCount; i++) {
+            Item item = items[i];
+            System.out.println("상품명: " + item.getName() + ", 합계: " + item.getTotalPrice());
+        }
+        System.out.println("전체 가격 합: " + calculateTotalPrice());
+    }
+
+    private int calculateTotalPrice() {
+        int totalPrice = 0;
+        for (int i = 0; i < itemCount; i++) {
+            Item item = items[i];
+            totalPrice += item.getTotalPrice();
+        }
+        return totalPrice;
+    }
+}
